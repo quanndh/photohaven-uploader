ec4a67334bee21873aefcdf52adb08b05d1110cc
Lucas Nguyen <lucas@miyagami.com>
2025-07-27 23:49:21 +0700

    init

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  WATCH_FOLDER: path.join(__dirname, "../Upload_Success"),
+  WATCH_FOLDER: path.join(__dirname, "../Upload_Sucess"),
   DRIVE_FOLDER_NAME: "Uploaded output",
   SYNC_DELAY_MINUTES: 5,
   CLEANUP_INTERVAL_MINUTES: 15,
